3bd25e4c95b185ebf5905c702dcf8ca089cea5e6
PlayerForever <chyneya@gmail.com>
2019-04-25 13:51:44 +0800

    object instantiation

---
+++ ob5_Instantiation.py
@@ -0,0 +1,16 @@
+class Programmer(object):
+
+    def __new__(cls, *args, **kwargs):
+        print('call __new__ method')
+        print(args)
+        return super(Programmer, cls).__new__(cls)
+
+    def __init__(self, name, age):
+        print('call __init__ method')
+        self.name = name
+        self.age = age
+
+
+if __name__ == '__main__':
+    programmer = Programmer('Jia Hu', 27)
+    print(programmer.__dict__)
